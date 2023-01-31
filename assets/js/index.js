779af79c3ab7513c7f680bf9b8d5731a4e3c3dc9
IsabelaMaroto <belamaroto@gmail.com>
2023-01-31 16:37:35 -0300

    completed project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,32 @@
+const showAnswer = document.querySelector('#advice')
+const showId = document.querySelector('#id')
+const button = document.getElementById('button')
+
 async function getAPI(){
-    const showAnswer = document.querySelector('#advice')
-    const showId = document.querySelector('#id')
-    const button = document.getElementById('button')
-
     //pegar API
-    try{
-        const api = await fetch('https://api.adviceslip.com/advice')
-        const answer= await  api.json()
-        console.log(answer)
-    
+    const api = await fetch('https://api.adviceslip.com/advice')
+    const answer= await  api.json()
+    console.log(answer)
+    showId.textContent = `${answer.slip.id}`
+    showAnswer.textContent = ` “${answer.slip.advice}”`
+}
 
-    //escutar botao
+function Listener() {
+      //escutar botao
     button.addEventListener("click", async () => {
         console.log('clicou')
-        showAdvice()
+        getAPI()
+        Show()
     })
+}
 
-    //exibir resposta
-    const showAdvice = () => {
+function Show(answer) {
+     //exibir resposta
+    return () => {
         showId.textContent = `${answer.slip.id}`
         showAnswer.textContent = ` “${answer.slip.advice}”`
-    }
-
-    }catch{
-        console.log("mensagem de erro")
     }
 }
 
 getAPI()
+Listener()
