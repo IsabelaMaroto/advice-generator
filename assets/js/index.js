e6ecf04f0428d12e74bcd602f58bc5ef92f8ab44
IsabelaMaroto <belamaroto@gmail.com>
2023-01-31 13:57:52 -0300

    Finished the html and JS

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+async function getAPI(){
+    const showAnswer = document.querySelector('#advice')
+    const showId = document.querySelector('#id')
+    const button = document.getElementById('button')
+
+    //pegar API
+    try{
+        const api = await fetch('https://api.adviceslip.com/advice')
+        const answer= await  api.json()
+        console.log(answer)
+    
+
+    //escutar botao
+    button.addEventListener("click", async () => {
+        console.log('clicou')
+        showAdvice()
+    })
+
+    //exibir resposta
+    const showAdvice = () => {
+        showId.textContent = `${answer.slip.id}`
+        showAnswer.textContent = `“${answer.slip.advice}”`
+    }
+
+    }catch{
+        console.log("mensagem de erro")
+    }
+}
+
+getAPI()
